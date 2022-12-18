bb366a81a540638c16d1d191c919a791f8cb52b7
Еlena Anisimova <elena.anis.12@yandex.ru>
2022-12-18 19:31:34 +0300

    feat: add angleBetweenClockHands solution

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -76,8 +76,12 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const hour = (endDate.getHours() - startDate.getHours()).toString().padStart(2, '0');
+  const minutes = (endDate.getMinutes() - startDate.getMinutes()).toString().padStart(2, '0');
+  const seconds = (endDate.getSeconds() - startDate.getSeconds()).toString().padStart(2, '0');
+  const miliSeconds = (endDate.getMilliseconds() - startDate.getMilliseconds()).toString().padStart(3, '0');
+  return `${hour}:${minutes}:${seconds}.${miliSeconds}`;
 }
 
 
@@ -97,10 +101,15 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
-}
+function angleBetweenClockHands(date) {
+  const hour = date.getUTCHours() % 12;
+  const minute = date.getUTCMinutes();
+  let angle = Math.abs(0.5 * (60 * hour - 11 * minute));
 
+  if (angle > 180) angle = 360 - angle;
+
+  return Math.abs((angle / 180) * Math.PI);
+}
 
 module.exports = {
   parseDataFromRfc2822,
