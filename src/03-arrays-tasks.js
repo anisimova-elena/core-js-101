326a621de47ce51b83b794cd9c21c8391686be6b
Еlena Anisimova <elena.anis.12@yandex.ru>
2022-12-18 14:27:39 +0300

    feat: add sortCitiesArray solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -461,8 +461,14 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  function compare(a, b) {
+    if (a.country < b.country || (a.country === b.country && a.city < b.city)) {
+      return -1;
+    }
+    return 1;
+  }
+  return arr.sort(compare);
 }
 
 /**
