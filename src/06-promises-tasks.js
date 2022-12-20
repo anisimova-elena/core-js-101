493ab4e8587c517c9d12597ebca2d0ac6021bd8f
Еlena Anisimova <elena.anis.12@yandex.ru>
2022-12-20 11:34:04 +0300

    feat: add willYouMarryMe solution

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,17 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  const promise = new Promise((resolve, reject) => {
+    if ((typeof isPositiveAnswer !== 'boolean') || (arguments.length === 0)) {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+    if (isPositiveAnswer) {
+      resolve('Hooray!!! She said "Yes"!');
+    } else resolve('Oh no, she said "No".');
+  });
+
+  return promise;
 }
 
 
